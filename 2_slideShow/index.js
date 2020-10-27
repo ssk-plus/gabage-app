ddfeac1fd362794ab8d49609be420779a0d1e03e
a-ssk <fabledfabled@gmail.com>
2020-10-27 22:10:27 +0900

    add 2_slideShowを追加

---
+++ 2_slideShow/index.js
@@ -0,0 +1,23 @@
+jQuery(function($){
+
+	let imgs = $('.slider img');
+	let page = 0;
+	let lastPage = imgs.length;
+
+  $(document).ready(function(){
+		imgs.eq(page).removeClass('slide');
+	});
+
+	$(document).on('click', '.slide-back', function() {
+		imgs.eq(page).addClass('slide');
+		page = page - 1;
+		imgs.eq(page).removeClass('slide');
+	});
+
+	$(document).on('click', '.slide-next', function() {
+		imgs.eq(page).addClass('slide');
+		page = page + 1;
+		imgs.eq(page).removeClass('slide');
+	});
+
+});
